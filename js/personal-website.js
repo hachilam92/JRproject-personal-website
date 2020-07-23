2d296149f514e4ebd0197fa84ae1d8715732a29b
Ziwei Lan <ziweilan92@gmail.com>
2020-07-23 15:39:41 +1000

    Add js functions to switch pages

--- js/personal-website.js
+++ js/personal-website.js
@@ -1,19 +1,25 @@
-const navItemList = Array.from(document.querySelectorAll('.nav-item'));
-const pageList = Array.from(document.querySelectorAll('.card'));
+window.onload = function(){
+    const navItemList = Array.from(document.querySelectorAll('.nav-item'));
+    const pageList = Array.from(document.querySelectorAll('.card'));
+    
+    const eventAdd = function(){
+        navItemList.forEach((item) => {
+            item.addEventListener('click', function(e){
+                navItemList.forEach((navItem)=>{
+                    navItem.classList.remove('active');
+                })
+                e.target.classList.add('active');
+                pageList.forEach((page)=> page.classList.remove('current-page'));
+                let pageName = e.target.textContent.toLowerCase().trim();
+                let page = document.querySelector(`.card-${pageName}`);
+                page.classList.add('current-page');
+            });
+        })
+    }
 
-const eventAdd = function(){
-    navItemList.forEach((item) => {
-        item.addEventListener('click', function(e){
-            navItemList.forEach((navItem)=>{
-                navItem.classList.remove('.active');
-            })
-            e.target.classList.add('.active');
-            pageList.forEach((page)=> page.classList.remove('current-page'));
-            let pageName = e.target.innerHTML.toLowerCase;
-            let page = document.getElementById(`card-${pageName}`);
-            page.classList.add('current-page');
-        });
-    })
+    eventAdd();
 }
+
+
 
 
