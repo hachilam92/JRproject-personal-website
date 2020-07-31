23cd7e58568884ffab8bf6b880c494806814dc90
Ziwei Lan <ziweilan92@gmail.com>
2020-07-31 20:47:21 +1000

    add alerts for inputs in contact page

--- js/personal-website.js
+++ js/personal-website.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     const navItemList = Array.from(document.querySelectorAll('.nav-item'));
     const pageList = Array.from(document.querySelectorAll('.card'));
+    const contactInfoList = Array.from(document.querySelectorAll('.contact-info'));
     
     function navAdd () {
         navItemList.forEach((item) => {
@@ -14,7 +15,7 @@ window.onload = function(){
                 let page = document.querySelector(`.${pageName}`);
                 page.classList.add('current-page');
             });
-        })
+        });
     }
 
     function contactSubmit () {
@@ -24,8 +25,29 @@ window.onload = function(){
         });
     }
 
+    function contactInfo () {
+        contactInfoList.forEach((item) => {
+            item.addEventListener('input', function(e){
+                let input = e.target;
+                let alert = document.querySelector(`.${input.id}-alert`);
+                let label = document.querySelector(`[for=${input.id}]`);
+                console.log(label);
+                if(input.value.length === 0){
+                    alert.classList.remove('empty');
+                    label.classList.remove('hoist');
+                }else{
+                    alert.classList.add('empty');
+                    label.classList.add('hoist');
+                    console.log(label);
+                }
+            });
+        });
+    }
+
+
     navAdd();
     contactSubmit();
+    contactInfo ();
 }
 
 
